429ce4841bbb2b4f009740a0c7f5a861abc61557
miracle <handong@onemt.com.cn>
2019-03-18 11:23:31 +0800

    新增Vue过滤器

--- src/main.js
+++ src/main.js
@@ -6,9 +6,20 @@ import router from './router'
 import store from './store'
 import ApiService from '@/common/api.service'
 
+import DateFilter from './common/date.filter'
+import ErrorFilter from './common/error.filter'
+import {CHECK_AUTH} from './store/actions.type'
+
 Vue.config.productionTip = false
+Vue.filter('date', DateFilter)
+Vue.filter('date', DateFilter)
+Vue.filter('error', ErrorFilter)
 
 ApiService.init()
+
+router.beforeEach((to, from, next) =>
+  Promise.all([store.dispatch(CHECK_AUTH)]).then(next)
+)
 
 /* eslint-disable no-new */
 new Vue({
