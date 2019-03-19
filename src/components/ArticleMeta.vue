8dcf775da0160698fd2226a2cff722b5bdef786e
miracle <handong@onemt.com.cn>
2019-03-19 19:25:50 +0800

    添加文章元数据视图部分代码

---
+++ src/components/ArticleMeta.vue
@@ -0,0 +1,78 @@
+<template>
+  <div class="article-meta">
+    <router-link
+      :to="{ name: 'profile', params: {username: article.author.username }}"
+    >
+      <img :src="article.author.image"/>
+    </router-link>
+    <div class="info">
+      <router-link
+        :to="{name: 'profile', params: { username: article.author.username }}"
+        class="author"
+      >
+        {{ article.author.username }}
+      </router-link>
+      <span class="date">{{ article.createdAt | date}}</span>
+    </div>
+  </div>
+  <template v-if="actions">
+  </template>
+  <template v-else>
+    <button
+      class="btn btn-sm pull-xs-right"
+      v-if="!actions"
+      v-on:click="toggleFavorite"
+      :class="{
+        'btn-primary' : article.favorited,
+        'btn-outline-primary': !article.favorited
+      }"
+    >
+      <i class="ion-heart"></i>
+      <span class="counter"> {{ article.favoritesCount }}</span>
+    </button>
+  </template>
+</template>
+
+<script>
+import {mapGetters} from 'vuex'
+import {FAVORITE_ADD, FAVORITE_REMOVE} from '../store/actions.type'
+
+export default {
+  name: 'DmArticleMeta',
+  components: {},
+  props: {
+    article: {
+      type: Object,
+      required: true
+    },
+    actions: {
+      type: Boolean,
+      required: false,
+      default: false
+    }
+  },
+  computed: {
+    ...mapGetters(['currentUser', 'isAuthenticated'])
+  },
+  methods: {
+    isCurrentUser () {
+      if (this.currentUser.username && this.article.author.username) {
+        return this.currentUser.username === this.article.author.username
+      }
+      return false
+    },
+    toggleFavorite () {
+      if (!this.isAuthenticated) {
+        this.$router.push({name: 'login'})
+        return
+      }
+      const action = this.article.favorited ? FAVORITE_REMOVE : FAVORITE_ADD
+      this.$store.dispatch(action, this.article.slug)
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
